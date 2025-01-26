793fcdaf92f67860c3b6b6f043cb955b4771f7cf
Peter <peterchinokoo@gmail.com>
2025-01-26 21:50:41 +0200

    change the url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 //middlewares
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://mens-wear-2lmy.onrender.com", "http://localhost:5173/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
