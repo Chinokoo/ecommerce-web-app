0d99ee7ea744d0db88f8e52a3cb42f1e085e33c6
Peter <peterchinokoo@gmail.com>
2025-01-26 21:57:22 +0200

    again

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(
   cors({
     origin: ["https://mens-wear-2lmy.onrender.com", "http://localhost:5173/"],
-    credentials: true,
+    //credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
