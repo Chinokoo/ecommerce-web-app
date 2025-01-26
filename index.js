d94ce9f0aa61fa49854d217de75e0a029dc3eb8d
Peter <peterchinokoo@gmail.com>
2025-01-26 21:17:06 +0200

    add production url to site

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 //middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://mens-wear-2lmy.onrender.com/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
